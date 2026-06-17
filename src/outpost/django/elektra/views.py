791dd82c643fab05c0523f7403573a2a71d5a788
Lukas Klavzer <lukas.klavzer@gmail.com>
2026-06-17 12:26:49 +0200

    Add permission classes to ProjectImportView for authentication

--- src/outpost/django/elektra/views.py
+++ src/outpost/django/elektra/views.py
@@ -1,12 +1,16 @@
 from django.core.cache import cache
 from django.http import HttpResponse
 from rest_framework.views import APIView
+from rest_framework import permissions
 
 from .conf import settings
 from .tasks import ElektraTasks
 
 
 class ProjectImportView(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+    
     def get(self, request):
         response = HttpResponse()
         response["Content-Type"] = "application/xml"
