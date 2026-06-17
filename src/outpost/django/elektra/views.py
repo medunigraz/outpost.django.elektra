0532abfd3d3cd286698336087eacec2d938d01d4
Lukas Klavzer <lukas.klavzer@gmail.com>
2026-06-17 11:22:02 +0200

    Implement Project Import functionality with XML generation and caching

---
+++ src/outpost/django/elektra/views.py
@@ -0,0 +1,17 @@
+from django.core.cache import cache
+from django.http import HttpResponse
+from rest_framework.views import APIView
+
+from .conf import settings
+from .tasks import ElektraTasks
+
+
+class ProjectImportView(APIView):
+    def get(self, request):
+        response = HttpResponse()
+        response["Content-Type"] = "application/xml"
+        xml = cache.get(settings.ELEKTRA_PROJECT_IMPORT_CACHE_KEY, None)
+        if not xml:
+            xml = ElektraTasks().generate()
+        response.write(xml)
+        return response
